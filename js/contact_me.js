633452335830861924ff39f9dede933b0079179c
febil va <febil.tvr@gmail.com>
2016-11-08 18:57:47 +0530

    change mail server http to https

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,8 +18,9 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                dataType: 'jsonp',
-                url: "http://getsimpleform.com/messages/ajax?form_api_token=9b775b39044c26ce328b6d56663e12e8",
+                dataType: "json",
+                url: "https://formspree.io/febil.tvr@gmail.com",
+                method:'POST',
                 data: {
                     name: name,
                     phone: phone,
